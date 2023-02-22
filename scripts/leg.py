9b5c162f1be71e55e53c426168517b6ed281faab
Lucas Vance <lucas.vance@protonmail.com>
2023-02-22 10:40:38 -0800

    Update variables

--- scripts/leg.py
+++ scripts/leg.py
@@ -9,21 +9,21 @@ leg2 = float(input("Leg 2:      "))
 hypotenuse = float(input("Hypotenuse: "))
 
 if leg1 == "":
-    toFind = "leg1"
+    to_find = "leg1"
 elif leg2 == "":
-    toFind = "leg2"
+    to_find = "leg2"
 elif hypotenuse == "":
-    toFind = "hypotenuse"
+    to_find = "hypotenuse"
 else:
     print("An error occurred")
 
-print("Finding " + toFind + ":")
+print("Finding " + to_find + ":")
 
-if toFind == "leg1":
+if to_find == "leg1":
     print(math.sqrt((hypotenuse * hypotenuse) - (leg2 * leg2)))
-elif toFind == "leg2":
+elif to_find == "leg2":
     print(math.sqrt((hypotenuse * hypotenuse) - (leg1 * leg1)))
-elif toFind == "hypotenuse":
+elif to_find == "hypotenuse":
     print(math.sqrt((leg1 * leg1) + (leg2 * leg2)))
 else:
     print("u did something wrong and ima let u figure it out. not me.")
