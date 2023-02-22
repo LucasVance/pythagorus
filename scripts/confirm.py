759fc4c3dd6b6ab3a6678b5ef6512ce57b408a09
Lucas Vance <lucas.vance@protonmail.com>
2023-02-22 10:19:50 -0800

    Add code to confirm.py

--- scripts/confirm.py
+++ scripts/confirm.py
@@ -1,1 +1,14 @@
-# Find if a triangle is a right triangle, given lengths of sides
+# Find if a triangle is a right, acute, or obtuse triangle, given lengths of 3 sides
+
+leg1 = float(input("Leg 1:      "))
+leg2 = float(input("Leg 2:      "))
+hypotenuse = float(input("Hypotenuse: "))
+
+if ((leg1 * leg1) + (leg2 * leg2) == hypotenuse * hypotenuse):
+    print("Right triangle")
+elif ((leg1 * leg1) + (leg2 * leg2) > hypotenuse * hypotenuse):
+    print("Acute triangle")
+elif ((leg1 * leg1) + (leg2 * leg2) < hypotenuse * hypotenuse):
+    print("Obtuse triangle")
+else:
+    print("An error occurred")
