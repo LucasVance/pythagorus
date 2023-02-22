001abed2450722a4885fca12b2f1bd15f8ad6a96
Lucas Vance <lucas.vance@protonmail.com>
2023-02-22 10:47:10 -0800

    Fix float() bug in leg.py

--- scripts/leg.py
+++ scripts/leg.py
@@ -4,20 +4,26 @@ import math
 
 print("Input two sides: leave the property blank that you wish to find")
 
-leg1 = float(input("Leg 1:      "))
-leg2 = float(input("Leg 2:      "))
-hypotenuse = float(input("Hypotenuse: "))
+leg1 = input("Leg 1:      ")
+leg2 = input("Leg 2:      ")
+hypotenuse = input("Hypotenuse: ")
 
 if leg1 == "":
     to_find = "leg1"
+    leg2 = float(leg2)
+    hypotenuse = float(hypotenuse)
 elif leg2 == "":
     to_find = "leg2"
+    leg1 = float(leg1)
+    hypotenuse = float(hypotenuse)
 elif hypotenuse == "":
     to_find = "hypotenuse"
+    leg1 = float(leg1)
+    leg2 = float(leg2)
 else:
     print("An error occurred")
 
-print("Finding " + to_find + ":")
+print(to_find + ":")
 
 if to_find == "leg1":
     print(math.sqrt((hypotenuse * hypotenuse) - (leg2 * leg2)))
